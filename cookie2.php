6d91659994707b779cec60553451ab309ef133f6
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-02-05 15:38:29 +0530

    Add files via upload

---
+++ cookie2.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_COOKIE["count"]))
+{
+    $count=$_COOKIE["count"]+1;
+}
+else
+    {
+        $count=1;
+    }
+    setcookie("count",$count,time()+3600);
+    echo "page count is: ".$count;
+
+?>
