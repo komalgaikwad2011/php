57ef5a11a01886fea2e25e94360f9b300424f5e0
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-02-07 08:12:40 +0530

    Add files via upload

---
+++ emp.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="POST"action="emp1.php">
+            enter emp no<input type="text"name="t1"><br>
+            enter emp name<input type="text"name="t2"><br>
+            enter emp addr<input type="text"name="t3"><br>
+            <input type="submit"value="submit">
+            </form>
+        </body>
+    </html>
