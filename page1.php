86f04d89619d74813d18e1b6c3b4a924035695ed
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-02-11 21:04:03 +0530

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="POST"action="page2.php">
+        enter customer no:<input type="text"name="t1"><br>
+        enter customer name:<input type="text"name="t2"><br>
+        enter customer addr:<input type="text"name="t3"><br>
+        <input type="submit"value="submit"><br>
+    </form>
+</body>
+</html>
