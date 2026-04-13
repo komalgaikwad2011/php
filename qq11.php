31356b8f18f6dc3e9c933e1e1f39d50dfdcf23e3
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-04-13 08:57:23 +0530

    Add files via upload

--- qq11.php
+++ qq11.php
@@ -6,21 +6,21 @@ $doc->appendChild($bookinfo);
 
 $book1=$doc->createElement("book");
 
-$book1->appendChild($xml->createElement("bookno",1));
-$book1->appendChild($xml->createElement("bookname","java"));
-$book1->appendChild($xml->createElement("authorname","balguru swami"));
-$book1->appendChild($xml->createElement("price",250));
-$book1->appendChild($xml->createElement("year",2023));
+$book1->appendChild($doc->createElement("bookno",1));
+$book1->appendChild($doc->createElement("bookname","java"));
+$book1->appendChild($doc->createElement("authorname","balguru swami"));
+$book1->appendChild($doc->createElement("price",250));
+$book1->appendChild($doc->createElement("year",2023));
 
 $bookinfo->appendChild($book1);
 
 $book2=$doc->createElement("book");
 
-$book2->appendChild($xml->createElement("bookno",2));
-$book2->appendChild($xml->createElement("bookname","php"));
-$book2->appendChild($xml->createElement("authorname","balguru"));
-$book2->appendChild($xml->createElement("price",270));
-$book2->appendChild($xml->createElement("year",2024));
+$book2->appendChild($doc->createElement("bookno",2));
+$book2->appendChild($doc->createElement("bookname","php"));
+$book2->appendChild($doc->createElement("authorname","balguru"));
+$book2->appendChild($doc->createElement("price",270));
+$book2->appendChild($doc->createElement("year",2024));
 
 $bookinfo->appendChild($book2);
 
