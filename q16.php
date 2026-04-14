7e0c2dd2297e94a05949eab26b9ce351ec562bd7
Komal Gaikwad <163570757+komalgaikwad2011@users.noreply.github.com>
2026-04-14 07:58:11 +0530

    Add files via upload

---
+++ q16.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <script type="text/javascript">
+            var a=confirm("exams are near have u prepare for it");
+            if(a==true)
+            {
+                var p=window.prompt("enter ur name");
+            if(p!=null)
+            {
+                window.alert("best of luck " + p);
+            }
+            else
+            {
+                window.alert("prepare for it");
+            }
+        }
+            </script>
+</body>
+</html>
---
+++ q17.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script>
+$(document.ready(function())
+{
+$("p").append("hello");
+$("ol").append("<li>apple</li>");
+});
+ </script>
+  </head>
+
+
+
+
+<body>
+    <p>this is paragraph tag</p>
+    <ol>
+        <li>mango</li>  
+     </ol>
+</body>
+
+</html>
